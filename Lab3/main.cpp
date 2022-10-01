ab78c2c64124dd1a0ef4d98afeb8f1b62ca3ca56
ramirez106 <112105048+ramirez106@users.noreply.github.com>
2022-09-30 19:32:09 -0500

    change

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -38,24 +38,17 @@ int main()
 	infile.open(inputfile);
 	outfile.open(outputfile);
 
-	//brings the file input and puts it into integers
 	infile >> fInput1 >> fInput2 >> fInput3 >> fInput4;
 
-	//outputs directions to the screen
 	cout << "Input four numbers, with a space inbetween each number" << endl;
-	cin >> one >> two >> three >> four; //assigns integers to input
+	cin >> one >> two >> three >> four;
 
-	/*displays the mean and standard deviation from the input and and input file
-	on the screen, the first three lines refer to user input and the second three lines
-	refer to the file input
-	*/
 	cout << "The mean of your numbers is:" << " " << mean(one, two, three, four) << endl;
 	cout << "The standard deviation of your numbers is:" << " " << sd(one, two, three, four, mean(one, two, three, four)) << endl;
 	cout << "\n" << "The numbers inputted by the files are:" << " " << fInput1 << "," << " " << fInput2 << "," << " " << fInput3 << "," << " " << fInput4 << endl;
 	cout << "The mean of the input file is:" << " " << mean(fInput1, fInput2, fInput3, fInput4) << endl;
 	cout << "The standard deviation of the file input is:" << " " << sd(fInput1, fInput2, fInput3, fInput4, mean(fInput1, fInput2, fInput3, fInput4)) << endl;
 
-	// closes the input and output file
 	infile.close();
 	outfile.close();
 
@@ -63,13 +56,11 @@ int main()
 
 }
 
-//function definition of our mean
 float mean(int one, int two, int three, int four)
 {
 	return float(one + two + three + four) / float(4);
 }
 
-//This is the function definition of standard deviation
 float sd(int one, int two, int three, int four, float mean)
 {
 	return sqrt((pow((one - mean), 2) + pow((two - mean), 2) + pow((three - mean), 2) + pow((four - mean), 2)) / 4);
